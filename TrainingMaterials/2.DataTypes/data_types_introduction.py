b38c13db2fc68d379404ee8e5c09bdca9ed40a24
caavram <fortech.avram@extaccount.com>
2024-01-31 14:27:03 +0200

    Added data types introduction

---
+++ TrainingMaterials/2.DataTypes/data_types_introduction.py
@@ -0,0 +1,35 @@
+# DATA TYPES
+"""
+Topics covered: 
+Basic data types: integer, float, string. 
+Type casting concept.
+Advance data types: list, tuple, dictionary
+REFERENCE https://www.w3schools.com/python/python_datatypes.asp
+"""
+
+# Integers
+# Numbers with no decimals like -5, -3, 0, 1, 2, etc.
+userAge = 20
+mobileNumber = 123456789
+print("User age:", userAge, "Mobile numer:", mobileNumber)
+
+# Float
+# Numbers with decimal parts like  1.234, -0.023, 12.01
+userHeight = 1.82
+userWeight = 67.2
+print("User height:", userHeight, "User weight:", userWeight)
+
+# String
+# REFERENCE https://www.w3schools.com/python/python_strings.asp
+# Refers to text
+userName = "O'Brian"
+userSurname = "Peter"
+userAge2 = "30"
+print("User name:", userName, "User surname:", userSurname)
+print("User age:", userAge2)
+print("Peter" + "Lee")
+
+# Built-in String Functions
+print("Peter".upper())
+print("Peter".lower())
+print(len("Peter"))
