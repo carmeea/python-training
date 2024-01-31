5b33e217da1b7a8307db85bc5833ee129a17aa97
AndreiEnea <andrei.enea@boomiteam.com>
2024-01-31 14:06:05 +0200

    added homework for working with files

---
+++ TrainingMaterials/Homework/Files/work_with_files_homework.py
@@ -0,0 +1,21 @@
+'''
+Examples to be used 👍
+I add some Bonus challenge - here we can identify some good “pythonists “ 😂
+
+1. Open/Read Text Files (Using For Loop)
+Create a Python script that opens a text file and reads its contents line by line using a for loop. The script should print each line to the console. Include error handling to manage situations where the file might not exist.
+Bonus Challenge: Extend the script to count the number of words in each line and display this count alongside each line.
+
+2. Write to a Text File
+Write a Python program that takes user input in a loop and writes each input as a new line in a text file. The program should continue taking input until the user types 'exit'. Ensure that the file is appropriately closed after writing.
+Bonus Challenge: Modify the program to check if the file already exists. If it does, append the new lines to the existing content; if not, create a new file.
+
+3. Working with Buffer Size and Binary Files
+Create a Python script that copies a binary file (like an image or a PDF) from one location to another using a specific buffer size. The buffer size should be a parameter that can be adjusted. The script should measure and display the time taken to copy the file with different buffer sizes.
+Bonus Challenge: Enhance the script to compare the checksums of the original and copied files to ensure the integrity of the copy.
+
+4. Delete/Rename Files
+Write a Python program that lists all files in a given directory and allows the user to either delete a file or rename a file. The user should specify the action and the target file through input.
+Bonus Challenge: Extend the program to support batch renaming of multiple files following a specific pattern .
+
+'''
