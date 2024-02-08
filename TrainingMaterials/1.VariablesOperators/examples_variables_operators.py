bd75e9c198aec9f2bdf317aad66b5ffa78421c13
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2024-02-08 16:12:10 +0200

    new changes

--- TrainingMaterials/1.VariablesOperators/examples_variables_operators.py
+++ TrainingMaterials/1.VariablesOperators/examples_variables_operators.py
@@ -5,6 +5,7 @@ print(project_name)
 
 # Calculate the total revenue from two products, A and B, sold last month.
 # Product A sold 150 units at $20 each, and Product B sold 300 units at $15 each.
+# Changed file
 product_A_units = 150
 product_A_price = 20
 product_B_units = 300
