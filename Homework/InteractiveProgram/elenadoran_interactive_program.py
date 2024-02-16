0d1f815e408c33a03bf32c107f15eb5ecf5acfc8
elenadoran1984 <elena.doran1984@gmail.com>
2024-02-16 15:22:15 +0200

    add first theme

---
+++ Homework/InteractiveProgram/elenadoran_interactive_program.py
@@ -0,0 +1,12 @@
+"""
+Multi-Currency Converter
+Write a function that converts an amount from RON to NOK (Norwegian Kroner), to BGN (Bulgarian Lev),
+to HUN (Hungarian Forint), to EUR (Euro), and to USD (Us Dollars).
+Use exchange rates:
+    1 Lev = 2.55 RON
+    1 NOK = 0.43 RON
+    1 Forint = 0.013 RON
+    1 Euro = 4,98 RON
+    1 USD = 4,57 RON
+Convert 100 RON to both each currency and print the results, rounded to two decimal places.
+"""
