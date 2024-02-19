f7e78ec1135a803d0512819c5aa3d7750806aec7
lptrfl <petri.fulea@gmail.com>
2024-02-20 01:36:00 +0200

    Incerc ceva

--- Homework/InteractiveProgram/homework_interactive_program.py
+++ Homework/InteractiveProgram/homework_interactive_program.py
@@ -10,3 +10,27 @@ Use exchange rates:
     1 USD = 4,57 RON
 Convert 100 RON to both each currency and print the results, rounded to two decimal places.
 """
+
+
+class rates:
+    def __init__(self, currency, value):
+        self.currency = currency
+        self.value = value
+
+c1 = rates("Lev", 2.55)
+c2 = rates("NOK", 0.43)
+c3 = rates("Forint", 0.013)
+c4 = rates("Euro", 4.98)
+c5 = rates("USD", 4.57)
+
+choice = input("let me know to which currency you wish to convert, options are: LEV, NOK, Forint, Euro, USD")
+while choice in rates.currency:
+    amount = input(f"give me an ammount to change form RON to {choice}"
+else:
+    print("you have to choose again, please pick a currency from the mentioned ones")
+
+        def converter(self):
+        print(f"The RON amount given {amount} can be converted to {} {choice}")
+        
+
+amount = input("give me an amount for conversion")
