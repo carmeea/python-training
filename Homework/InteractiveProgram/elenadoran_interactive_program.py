08e4ac5d3b57174dcabe1dec500262d0305f2d6f
elenadoran1984 <elena.doran1984@gmail.com>
2024-02-19 14:22:51 +0200

    Add Homework for DataTypes and InteractiveProgram

--- Homework/InteractiveProgram/elenadoran_interactive_program.py
+++ Homework/InteractiveProgram/elenadoran_interactive_program.py
@@ -11,4 +11,24 @@ Use exchange rates:
 Convert 100 RON to both each currency and print the results, rounded to two decimal places.
 """
 
-#firsttest
+amount_ron = 100
+exchange_rate_lev = 2.55
+exchange_rate_nok = 0.43
+exchange_rate_forint = 0.013
+exchange_rate_euro = 4.98
+exchange_rate_usd = 4.57
+
+amount_lev = amount_ron / exchange_rate_lev
+print("Convert RON to lev: ", round(amount_lev, 2))
+
+amount_nok = amount_ron / exchange_rate_nok
+print("Convert RON to nok: ", round(amount_nok, 2))
+
+amount_forint = amount_ron / exchange_rate_forint
+print("Convert RON to forint: ", round(amount_forint, 2))
+
+amount_euro = amount_ron / exchange_rate_euro
+print("Convert RON to euro: ", round(amount_euro, 2))
+
+amount_usd = amount_ron / exchange_rate_usd
+print("Convert RON to usd: ", round(amount_usd, 2))
