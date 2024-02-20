a1007afa043a39cae9c4576d947cf2060b3bfc81
NicuP93 <iulianpiticari@yahoo.com>
2024-02-20 18:49:40 +0200

    tema partiala - data types

---
+++ Homework/DataTypes/nicupiticari_homework_data_types.py
@@ -0,0 +1,14 @@
+# Sales Report Processing
+#   You are given the data for quarter sales: sales = [120, 150, 170, 100].
+#       a. Calculate the total sales for the quarter.
+#       b. Calculate the average sales per month.
+#       c. Print both the total and the average, formatted to two decimal places.
+M1 = 120
+M2 = 150
+M3 = 170
+M4 = 100
+totalSales = M1 + M2 + M3 + M4
+avgSales = totalSales / 4
+print ("Total sales are " +str(totalSales))
+print ("Average sales are " +str(avgSales))
+print ("Total and average sales are " +str(totalSales) +" and " +str(avgSales)) # nu reusesc sa printez cu 2 zecimale, need help
