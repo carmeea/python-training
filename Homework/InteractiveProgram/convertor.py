f44375e8a86d995290d51bab839502ba40f6bfff
lptrfl <petri.fulea@gmail.com>
2024-02-21 17:56:45 +0200

    check for invalid amount

--- Homework/InteractiveProgram/convertor.py
+++ Homework/InteractiveProgram/convertor.py
@@ -8,13 +8,22 @@ for i in j["rates"]:
     print(f"1 {i['currency']} = {i['value']} RON")
 
 amount = input("What is the RON amount that you wish to exchange?\n")
-valuta = input("What is the currency you wish to exchange to?\n")
 
-for i in j["rates"]:
-    if i["currency"] == valuta:
-        conversie = int(amount) / float(i["value"])
-        print(f"The amount of {amount} RON will be converted to {conversie:.2f} {valuta}")
-else:
-    print("We can't exchange to the selected currency.")
+while int(amount) <= 0:
+    print("The amount specified is not valid!")
+    amount = input("Give us another amount that you would like to exchange\n")
+else:    
+    valuta = input("What is the currency you wish to exchange to?\n")
+    found = False
+
+    for i in j["rates"]:
+        if i["currency"] == valuta:
+            conversie = int(amount) / float(i["value"])
+            print(f"The amount of {amount} RON will be converted to {conversie:.2f} {valuta}")
+            found = True
+            break
+
+    if not found:
+        print("We can't exchange to the selected currency.")
 
 f.close()
