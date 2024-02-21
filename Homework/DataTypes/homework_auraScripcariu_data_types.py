a7d2a5bba156594eaec5d7769ba2abe3acbdad4d
Aura7790 <aura.bejenaru@yahoo.com>
2024-02-21 10:18:22 +0200

    hoemwork for data types and interactive program

---
+++ Homework/DataTypes/homework_auraScripcariu_data_types.py
@@ -0,0 +1,73 @@
+
+"""
+Sales Report Processing
+You are given the data for quarter sales: sales = [120, 150, 170, 100].
+a. Calculate the total sales for the quarter.
+b. Calculate the average sales per month.
+c. Print both the total and the average, formatted to two decimal places.
+"""
+sales = [120, 150, 170, 100]
+
+quarter_total_sales = sales[0] + sales[1] + sales[2] + sales[3]
+quarter_average_sales = int(quarter_total_sales) / int(4)
+print(f"Total sales for the quarter is: {quarter_total_sales}")
+print(f"Average sales for the quarter is: {quarter_average_sales}")
+# OR
+quarter_total_sales2 = sum(sales)
+quarter_average_sales2 = quarter_total_sales2 / int(4)
+print(f"Total sales for the quarter is: {quarter_total_sales2}")
+print(f"Average sales for the quarter is: {quarter_average_sales2}")
+# OR
+quarter_total_sales3 = sum(int(i) for i in sales)
+quarter_average_sales3 = quarter_total_sales3 / 4
+print(f"Total sales for the quarter is: {quarter_total_sales3}")
+print(f"Average sales for the quarter is: {quarter_average_sales3}")
+
+
+"""
+Read from input a string with your name.
+a. Create a dictionary for you as called 'employee' with department, 
+b. Add a new field 'email' with the value 'alice.finance@example.com'.
+c. Print the dictionary.
+"""
+name = input("Enter your name: ")
+
+department = {"employee": name}
+department["email"] = "alice.finance@example.com"
+# OR
+# contact = {"email": "alice.finance@example.com"}
+# department.update(contact)
+
+print(department)
+
+
+"""
+Create a tuple with the names of three conference rooms: 'Aspen', 'Birch', and 'Cedar'.
+a. Print the entire tuple.
+b. Ask the user to input the number (1, 2, or 3) corresponding to the room they want to book.
+c. Print a confirmation message including the name of the booked room.
+"""
+conference_rooms = ("Aspen", "Birch", "Cedar")
+print("Available conference rooms: ", conference_rooms)
+conference_room_number = input("Please enter 0 for Aspen, 1 for Birch or 2 for Cedar, to book your desired room: ")
+print("You have booked ", conference_rooms[int(conference_room_number)])
+# OR
+# print("Please enter 0 for Aspen, 1 for Birch or 2 for Cedar, to book your desired room")
+# print("You have booked ", conference_rooms[int(input())])
+
+
+"""
+Create a list of products: 'Laptop', 'Phone', 'Printer', 'Headset', 'Monitor'.
+a. The company has decided to remove 'Printer' from the inventory and add 'Tablet'.
+b. Update the list and print the updated inventory.
+c. Print the list
+"""
+
+products = ['Laptop', 'Phone', 'Printer', 'Headset', 'Monitor']
+# products.remove('Printer')
+# OR
+products.pop(2)
+print(products)
+# products.append('Tablet')
+products.insert(2, 'Tablet')
+print(products)
---
+++ Homework/InteractiveProgram/homework_auraScripcariu_interactive.py
@@ -0,0 +1,48 @@
+"""
+Multi-Currency Converter
+Write a function that converts an amount from RON to NOK (Norwegian Kroner), to BGN (Bulgarian Lev),
+to HUN (Hungarian Forint), to EUR (Euro), and to USD (Us Dollars).
+Use exchange rates:
+    1 Lev = 2.55 RON
+    1 NOK = 0.43 RON
+    1 Forint = 0.013 RON
+    1 Euro = 4,98 RON
+    1 USD = 4,57 RON
+Convert 100 RON to both each currency and print the results, rounded to two decimal places.
+"""
+
+
+# amount_ron = input("Insert the amount in RON: ")
+amount_ron = 100
+print("RON amount to the be exchanged: ", amount_ron)
+
+exchange_rate_nok = 0.43
+exchange_rate_bgn = 2.55
+exchange_rate_hun = 0.013
+exchange_rate_eur = 4.98
+exchange_rate_usd = 4.57
+
+amount_nok = (
+    int(amount_ron) / float(exchange_rate_nok)
+)
+print("Amount in Norvegian Kroner is:", round(amount_nok, 2))
+
+amount_bgn = (
+    int(amount_ron) / float(exchange_rate_bgn)
+)
+print("Amount in Bulgarian Lev is:", round(amount_bgn, 2))
+
+amount_hun = (
+    int(amount_ron) / float(exchange_rate_hun)
+)
+print("Amount in Hungarian Forint is:", round(amount_hun, 2))
+
+amount_eur = (
+    int(amount_ron) / float(exchange_rate_eur)
+)
+print("Amount in Euro is:", round(amount_eur, 2))
+
+amount_usd = (
+    int(amount_ron) / float(exchange_rate_usd)
+    )
+print("Amount in US dolars is:", round(amount_usd, 2))
