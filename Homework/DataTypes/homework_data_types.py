aa9bfbc1307f373b8d9ac064062025d0825d4c8d
petrutimisan <timishan@gmail.com>
2024-02-22 09:59:03 +0200

    homework

--- Homework/DataTypes/homework_data_types.py
+++ Homework/DataTypes/homework_data_types.py
@@ -5,6 +5,12 @@ a. Calculate the total sales for the quarter.
 b. Calculate the average sales per month.
 c. Print both the total and the average, formatted to two decimal places.
 """
+sales = [120, 150, 170, 100]
+
+totalSales = sum(sales)
+avgSales = totalSales/12
+print("%.2f" % totalSales)
+print("%.2f" % avgSales)
 
 """
 Read from input a string with your name.
@@ -12,17 +18,34 @@ a. Create a dictionary for you as called 'employee' with department,
 b. Add a new field 'email' with the value 'alice.finance@example.com'.
 c. Print the dictionary.
 """
-
+name = input ("Write your name: ")
+employee = {"name" : name, "department" : ""}
+employee["email"] = "alice.finance@example.com"
+print(employee)
 """
 Create a tuple with the names of three conference rooms: 'Aspen', 'Birch', and 'Cedar'.
 a. Print the entire tuple.
 b. Ask the user to input the number (1, 2, or 3) corresponding to the room they want to book.
 c. Print a confirmation message including the name of the booked room.
 """
+conferenceRooms = ("Aspen", "Birch", "Cedar")
 
+print("The conference rooms are:", conferenceRooms)
+
+chooseRoom = int(input("Choose one of the 3 rooms (0-2): "))
+
+try:
+    chosenRoom = conferenceRooms[chooseRoom]
+    print("You chose the room:", chosenRoom)
+except KeyError:
+    print("The number you introduced doesn't exist")
 """
 Create a list of products: 'Laptop', 'Phone', 'Printer', 'Headset', 'Monitor'.
 a. The company has decided to remove 'Printer' from the inventory and add 'Tablet'.
 b. Update the list and print the updated inventory.
 c. Print the list
 """
+productsList = ['Laptop', 'Phone', 'Printer', 'Headset', 'Monitor']
+del productsList[2]
+productsList.insert(2, 'Tablet')
+print(productsList)
--- Homework/InteractiveProgram/homework_interactive_program.py
+++ Homework/InteractiveProgram/homework_interactive_program.py
@@ -10,3 +10,19 @@ Use exchange rates:
     1 USD = 4,57 RON
 Convert 100 RON to both each currency and print the results, rounded to two decimal places.
 """
+def convertRon(amountRon):
+
+  exchangeRates = {
+        "LEV" : 2.55,
+        "NOK" : 0.43,
+        "BGN" : 0.013,  
+        "HUN" : 4.98,
+        "EUR" : 4.57,
+        "USD" : 4.57
+    }
+  
+  for currency, rate in exchangeRates.items():
+    convertedAmount = amountRon / rate
+    print(f"{amountRon} RON is equal to {convertedAmount:.2f} {currency}")
+
+convertRon(100)
