28418c76e129496b0a66c86ef2fa64f59458d886
carmeeaavram <carmeea.avram@qlik.com>
2024-02-22 11:45:18 +0200

    Added homework for conditional statements and exception handling

---
+++ Homework/ChoicesDecisions/homework_choices_decisions.py
@@ -0,0 +1,10 @@
+"""
+1. Write a program that finds all numbers which are divisible by 7 and multiples of 5,
+between 1500 and 2700 (both included). 
+Then accept a number from the user and verify if it is part of the found list numbers.
+Hint: use methods range(), input(), append(), try-except
+
+2. Write a program that accepts a word from the user and print it in reverse. 
+Catch any errors from user's input.
+Hint: use methods range(), input(), try-except
+"""
