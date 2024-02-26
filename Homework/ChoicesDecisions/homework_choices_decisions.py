0f558392083812e444720301c46b04618187b255
lptrfl <petri.fulea@gmail.com>
2024-02-26 11:41:43 +0200

    2nd part of hw

--- Homework/ChoicesDecisions/homework_choices_decisions.py
+++ Homework/ChoicesDecisions/homework_choices_decisions.py
@@ -11,7 +11,6 @@ Hint: use methods range(), input(), try-except
 """
 
 #1
-#found = True
 results = []
 for i in range(1500, 2700, 5):
     if i % 7 == 0:
@@ -28,3 +27,22 @@ try:
         print("number not in list")
 except ValueError:
     print("Invalid input. Please try again")
+
+#2.1
+user_word = input("give me an english word for printing it out in reverse\n")
+letters_list=list(user_word)
+#print(letters_list)
+reversed_word = ""
+
+try:
+    if user_word.isalpha():
+       letters_list.reverse()
+       #print(letters_list)
+       for i in range(len(user_word)):
+           reversed_word = reversed_word + letters_list[i]
+       print(reversed_word)
+    else:
+        print("given input is not a word in english")
+except ValueError:
+    print["Invalid input.Please try again"]
+        
