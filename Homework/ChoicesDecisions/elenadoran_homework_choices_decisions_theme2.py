048503cfe0a870607c75bea97c4f0451814d8f92
elenadoran1984 <elena.doran1984@gmail.com>
2024-02-28 15:24:27 +0200

    Add Homework for Choices Decisions

---
+++ Homework/ChoicesDecisions/elenadoran_homework_choices_decisions_theme2.py
@@ -0,0 +1,23 @@
+"""
+2. Write a program that accepts a word from the user and print it in reverse. 
+Catch any errors from user's input.
+Hint: use methods range(), input(), try-except
+"""
+
+
+def reverse_word(word):
+    word_reverse = ""
+    index = len(word)
+    try:
+        while index > 0:
+            word_reverse += word[index - 1]
+            index = index - 1
+        user_input = word
+        if not user_input:
+            raise ValueError("Error: You did not enter a word")
+    except ValueError as e:
+        print(e)
+    return word_reverse
+
+
+print(reverse_word(input("Enter a word: ")))
