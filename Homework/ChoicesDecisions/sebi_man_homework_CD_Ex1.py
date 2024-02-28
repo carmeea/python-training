1f1165c82c87f85b414cc79c2662f0e82bdd998a
msebiandrei <mansebia@gmail.com>
2024-02-28 10:47:42 +0200

    Sebi Man - Homework CD

--- Homework/ChoicesDecisions/sebi_man_homework_CD_Ex1.py
+++ Homework/ChoicesDecisions/sebi_man_homework_CD_Ex1.py
@@ -4,9 +4,7 @@ for x in numbers:
     if (x % 5 == 0) & (x % 7 == 0):
         print(x)
         numbers_list.append(x)
-        x+=1
-    else:
-        x+=1
+    x+=1
 
 user_input = int(input("Please enter your number: "))
 if user_input in numbers_list:
