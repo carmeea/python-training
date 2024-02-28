deed1bf2552dbfebc460b018f8993067568b183a
rotestbert <rotestbert@gmail.com>
2024-02-28 23:00:27 +0200

    updates

--- Homework/ChoicesDecisions/robertlaszlo_choices_decisions.py
+++ Homework/ChoicesDecisions/robertlaszlo_choices_decisions.py
@@ -8,17 +8,17 @@ Hint: use methods range(), input(), append(), try-except
 numbers = []
 for num in range(1500, 2701):
     if num % 7 == 0 and num % 5:
-            numbers.append(num)
+        numbers.append(num)
 
 try:
     input = int(input("Enter number: "))
     input_number = int(input)
     if input_number in numbers:
         print("Number is divisible by 7 and is a multiple of 5")
-    elif input < 1500 or input > 2700:
-        print("Number is outside of the range!") 
+    elif input <= 1500 or input >= 2700:
+        print("Number is outside of the range!")
     else:
-        print("Not dividable by 7 and is a multiple of 5")      
+        print("Not dividable by 7 and is a multiple of 5")
 except ValueError:
     print("Invalid input, please enter an Integer Number")
 
