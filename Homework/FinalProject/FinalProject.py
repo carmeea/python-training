1a619a6ec4790a4b40ddaf114b1c9de9e4fb27ca
carmeea <avramcarmeea@gmail.com>
2024-03-06 00:14:36 +0200

    Added Final Project Details

---
+++ Homework/FinalProject/FinalProject.py
@@ -0,0 +1,4 @@
+"""
+Given the data.csv file, find products by subcategory. Subcategory will be given as user input.
+Write to another CSV file all found products. If subcategory is not found throw an error and do not create the file.
+"""
