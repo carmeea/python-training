9618db39d456976896a239fc667878bf80baf583
lptrfl <petri.fulea@gmail.com>
2024-03-07 14:45:33 +0200

    convertor file

--- Homework/InteractiveProgram/convertor.py
+++ Homework/InteractiveProgram/convertor.py
@@ -4,6 +4,7 @@ f = open("Homework/InteractiveProgram/rates.json")
 j = json.load(f)
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 print("Our current rates are:")
 for i in j["rates"]:
     print(f"1 {i['currency']} = {i['value']} RON")
@@ -29,14 +30,29 @@ else:
 =======
 print("our current rates are:")
 for i in j["value"]:
+=======
+print("Our current rates are:")
+for i in j["rates"]:
+>>>>>>> eff4ce9 (update convertor)
     print(i)
 
+amount = input("What is the RON amount that you wish to exchange?")
+valuta = input("What is the currency you wish to exchange to?")
 
+<<<<<<< HEAD
 # amount = input("what is the RON amount that you wish to exchange")
 # valuta = input("what is the currency you wish to exchange to")
 
 # for valuta in j["currency"]:
 #     print(f"The amount of {amount} RON will be converted to {amount/int(f["value"])}.2f {valuta}")
 >>>>>>> 7e879d6 (convertor cu rates din fisier)
+=======
+for i in j["rates"]:
+    if i["currency"] == valuta:
+        conversie = int(amount) / float(i["value"])
+        print(f"The amount of {amount} RON will be converted to {conversie:.2f} {valuta}")
+else:
+    print("We can't exchange to the selected currency.")
+>>>>>>> eff4ce9 (update convertor)
 
 f.close()
