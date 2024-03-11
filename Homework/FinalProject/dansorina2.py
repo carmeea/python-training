99aaf694aef4ebfea2191906bae0b9fb91f18c67
danpaun2021 <dan_paun2005@yahoo.com>
2024-03-11 18:34:35 +0200

    Update code for second function for DanSorina project

--- Homework/FinalProject/dansorina2.py
+++ Homework/FinalProject/dansorina2.py
@@ -2,18 +2,40 @@
 #( dictionarul are ca si prima cheie prima coloana din csv si ca valoare are tuple pentru restul coloanelor)
 import csv
 import os
-def csv_dictionary (file_path):
+# def csv_dictionary (file_path):
+#     product_dictionary = {}
+#     with open(file_path, 'r', newline='',encoding='utf-8') as file:
+#         reader = csv.reader(file)
+#         header = next(reader)
+#     for column in reader:
+#         primary_key = column[0]
+#         values_tuple = tuple(column[1:])
+#         product_dictionary[primary_key] = values_tuple
+#     return product_dictionary
+# data_dictionary = {}
+# path = "/home/dan/work/python-training/Homework/FinalProject/data.csv"
+# os.path.isfile(path)
+# data_dictionary = csv_dictionary(path)
+# print(data_dictionary)
+
+
+import csv
+import os
+
+def csv_dictionary(file_path):
     product_dictionary = {}
-    with open(file_path, 'r', newline='',encoding='utf-8') as file:
+    with open(file_path, 'r', newline='', encoding='utf-8') as file:
         reader = csv.reader(file)
         header = next(reader)
-    for column in reader:
-        primary_key = column[0]
-        values_tuple = tuple(column[1:])
-        product_dictionary[primary_key] = values_tuple
+        for column in reader: 
+            primary_key = column[0]
+            values_tuple = tuple(column[1:])
+            product_dictionary[primary_key] = values_tuple
     return product_dictionary
-data_dictionary = {}
-path = "/home/sorina/w/python-training/Homework/FinalProject/data.csv"
-os.path.isfile(path)
-data_dictionary = csv_dictionary(path)
-print(data_dictionary)
+
+path = "/home/dan/work/python-training/Homework/FinalProject/data.csv"
+if os.path.isfile(path):
+    data_dictionary = csv_dictionary(path)
+    print(data_dictionary)
+else:
+    print("File does not exist.")
