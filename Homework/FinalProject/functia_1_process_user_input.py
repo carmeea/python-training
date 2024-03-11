7b715acfdca1a6529f76515f941486193d0746f2
Skippy777 <paul.muresan.7@gmail.com>
2024-03-11 17:39:41 +0200

    Functia de procesare user input

---
+++ Homework/FinalProject/functia_1_process_user_input.py
@@ -0,0 +1,26 @@
+
+def process_user_input(userInput):
+    #userInput=input("Introduceti subcategoria: ")
+    checkOther=0
+    userInput=userInput.strip()
+         
+    try:
+        #if not all(userInput.isalpha() or char.isspace() for char in userInput):
+        for char in userInput:
+            if not (char.isalpha() or char.isspace()):
+               checkOther=checkOther+1
+        if checkOther>0:       
+            raise ValueError("Input should contain only text and spaces")
+        userInput = userInput.lower()
+        userInput = userInput.capitalize()
+        return userInput
+    except ValueError as e:
+        print(f"Error: {e}")
+        return None
+
+#user_input=""
+user_input=input("introduceti subcategoria: ")  
+result=process_user_input(user_input)
+#result=process_user_input()
+
+print("Processed input:-",result)
