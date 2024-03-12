152c2a605b7d8c15b55f7f5958ef21772206849d
alina.bentan <alina.bentan@gmail.com>
2024-03-12 09:49:31 +0200

    function3 updates 2

--- Homework/FinalProject/function3.py
+++ Homework/FinalProject/function3.py
@@ -1,11 +1,4 @@
-import urllib
-import pydoc
 
-# linkk = 'http://example.com/ceva/ceva2.html'
-# x= linkk.split('/')
-# print(x)
-# y=x[3]
-# print(y)
 
 dictionary = {'pufuleti cu sare': ['https://www.mega-image.ro/Dulciuri-si-snacks/Ciocolata/Batoane/Baton-de-ciocolata-cu-crema-de-rom-30g/p/28815', '100gr', 'calorii'], 'baton cu arahide': ['https://www.mega-image.ro/Dulciuri-si-snacks/Napolitane-biscuiti-si-prajituri/Prajituri-si-rulade/Prajitura-cu-lapte-30g/p/64686', '200gr', 'calorii mici']}
 def getSubcategory(dictionary):
