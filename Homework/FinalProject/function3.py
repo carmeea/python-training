ac2745662d18d088e605b0910db7ea8263cb83e7
alina.bentan <alina.bentan@gmail.com>
2024-03-12 09:48:20 +0200

    function3 updates

--- Homework/FinalProject/function3.py
+++ Homework/FinalProject/function3.py
@@ -7,11 +7,12 @@ import pydoc
 # y=x[3]
 # print(y)
 
-dictionary = {'pufuleti cu sare': [('https://www.mega-image.ro/Dulciuri-si-snacks/Ciocolata/Batoane/Baton-de-ciocolata-cu-crema-de-rom-30g/p/28815', '100gr', 'calorii')], 'baton cu arahide': [('https://www.mega-image.ro/Dulciuri-si-snacks/Napolitane-biscuiti-si-prajituri/Prajituri-si-rulade/Prajitura-cu-lapte-30g/p/64686', '200gr', 'calorii mici')]}
-def getSubcategory(subcategory):
+dictionary = {'pufuleti cu sare': ['https://www.mega-image.ro/Dulciuri-si-snacks/Ciocolata/Batoane/Baton-de-ciocolata-cu-crema-de-rom-30g/p/28815', '100gr', 'calorii'], 'baton cu arahide': ['https://www.mega-image.ro/Dulciuri-si-snacks/Napolitane-biscuiti-si-prajituri/Prajituri-si-rulade/Prajitura-cu-lapte-30g/p/64686', '200gr', 'calorii mici']}
+def getSubcategory(dictionary):
+    subcategoryList = []
     for val in (i[0] for i in dictionary.values()):
-        link = val[0]
-        segment = link.split('/')
-        subcategory =  segment[4]
-        return subcategory
-    
+        subcategory = val.split('/')[4]
+        subcategoryList.append(subcategory)
+    return subcategoryList
+
+print(getSubcategory(dictionary))
