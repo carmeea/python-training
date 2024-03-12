0c30b8db3aa9a9f866f693e7be6cecccae37b3b2
alina.bentan <alina.bentan@gmail.com>
2024-03-12 15:17:51 +0200

    fct4 - partial

--- Homework/FinalProject/function3.py
+++ Homework/FinalProject/function3.py
@@ -1,11 +1,21 @@
-
-
+#from functia_1_process_user_input import process_user_input
+userInput = 'Ciocolata'
 dictionary = {'pufuleti cu sare': ['https://www.mega-image.ro/Dulciuri-si-snacks/Ciocolata/Batoane/Baton-de-ciocolata-cu-crema-de-rom-30g/p/28815', '100gr', 'calorii'], 'baton cu arahide': ['https://www.mega-image.ro/Dulciuri-si-snacks/Napolitane-biscuiti-si-prajituri/Prajituri-si-rulade/Prajitura-cu-lapte-30g/p/64686', '200gr', 'calorii mici']}
 def getSubcategory(dictionary):
     subcategoryList = []
+    new_dict = {}
     for val in (i[0] for i in dictionary.values()):
         subcategory = val.split('/')[4]
         subcategoryList.append(subcategory)
-    return subcategoryList
+        if userInput != subcategory:
+            print(subcategory, userInput)
+            for key in dictionary.keys():
+                print(key)
+                new_dict.setdefault(key,[]).append(dictionary[key])
+                print(new_dict)
+    return new_dict
+#    return subcategoryList
+
+
 
 print(getSubcategory(dictionary))
