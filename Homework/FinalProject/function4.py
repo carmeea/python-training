d8ad44322ed284386d387d766a93868701fac9e5
Maria <condreamariaa@gmail.com>
2024-03-12 17:02:53 +0200

    "Integrated with function1"

--- Homework/FinalProject/function4.py
+++ Homework/FinalProject/function4.py
@@ -1,4 +1,5 @@
-userInput = "Ciocolata"
+from functia_1_process_user_input import result
+
 dictionary = {
     "pufuleti cu sare": [
         "https://www.mega-image.ro/Dulciuri-si-snacks/Ciocolata/Batoane/Baton-de-ciocolata-cu-crema-de-rom-30g/p/28815",
@@ -6,9 +7,14 @@ dictionary = {
         "calorii",
     ],
     "baton cu arahide": [
-        "https://www.mega-image.ro/Dulciuri-si-snacks/Napolitane-biscuiti-si-prajituri/Prajituri-si-rulade/Prajitura-cu-lapte-30g/p/64686",
+        "https://www.mega-image.ro/Dulciuri-si-snacks/Napolitane-biscuiti/Prajitura-cu-lapte-30g/p/64686",
         "200gr",
         "calorii mici",
+    ],
+    "Croissant cu crema de cacao 65g": [
+        "https://www.mega-image.ro/Dulciuri-si-snacks/Croissant si cola/Croissant-cu-crema-de-cacao-65g/p/5346",
+        "1000gr",
+        "199kcal",
     ],
 }
 
@@ -16,8 +22,8 @@ dictionary = {
 def getDictBySubcategory(dictionary):
     new_dict = {}
     for key, val in dictionary.items():
-        subcategory = "".join(val).split("/")[4]
-        if userInput == subcategory:
+        subcategory = "".join(val).split("/")[4].lower()
+        if result == subcategory:
             new_dict = {key: val}
     return new_dict
 
