3a7c0b38aa9069aebf302bcc0105aa2d2b69530a
lptrfl <petri.fulea@gmail.com>
2024-03-13 11:59:34 +0200

    2nd part of project

--- Homework/FinalProject/echipa1.py
+++ Homework/FinalProject/echipa1.py
@@ -1,23 +1,13 @@
 import csv
-import pandas as pd
 import pprint
-
-# subcategory = input("Enter a subcategory for the search: ")
+import pandas as pd
 
 subcategoryDict = {}
 base_category = "Dulciuri-si-snacks"
 with open("data.csv", "r") as datafile:
     reader=csv.DictReader(datafile)
-    #csvwriter = csv.writer(newfile)
-
-    for data in reader:
-        # if subcategory.lower() in data[1].lower():
-        #     csvwriter.writerow(data)
-        #     print("File created successfully")
-        #     print("File name: Filtered file.csv")
-        # else:
-        #     print("Subcategory not found")
-        #     break
+    
+    for data in reader: 
         split_link = data["link"].split("/")
         index = split_link.index(base_category) + 1
         #subcategoryDict[split_link[index]] = data
@@ -27,7 +17,16 @@ with open("data.csv", "r") as datafile:
         else:
             subcategoryDict[subcategory] = [data]
     #pprint.pprint(subcategoryDict)
-    print(subcategoryDict.keys())
-        
-
+    print(f"Subcategoriile pe care le puteti cauta sunt: {subcategoryDict.keys()}")
     
+    subcategory = input("Enter a subcategory for the search: ")
+    matching_key = next((key for key in subcategoryDict.keys() if key.lower() == subcategory.lower()), None)
+
+    if matching_key:
+        print(f"Subcategory {matching_key} found in the database")
+        print(f"File was created successfully and the name of the file is {matching_key}.csv")
+        df = pd.DataFrame(subcategoryDict[matching_key])
+        df.to_csv(f"{matching_key}.csv", index=False)
+    else:
+        print(f"Subcategory {matching_key} not found in the file")
+        print(f"No file was created")
