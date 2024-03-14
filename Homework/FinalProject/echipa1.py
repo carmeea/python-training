f087a661fa62f05fa3ff0ce011e0efc5bbb2d103
lptrfl <petri.fulea@gmail.com>
2024-03-14 13:29:10 +0200

    some changes

--- Homework/FinalProject/echipa1.py
+++ Homework/FinalProject/echipa1.py
@@ -8,17 +8,22 @@ subcategoryDict = {}
 base_category = "Dulciuri-si-snacks"
 
 #open the file and read the data
+#DictReader is used to create a dictionary with the data from the file
 with open("data.csv", "r") as datafile:
     reader=csv.DictReader(datafile)
 
-#iterate through the file and create a dictionary with the subcategories as keys and the values are lists containing the data
+#iterate through the dictonary created from the file and add to subcategoryDict the subcategory from the link as a key and the rest of the data as a value
     for data in reader:
-        #split the link to get the subcategory
+        
+        #create alist with the elements of the link split by "/"
         split_link = data["link"].split("/")
-        #get the index of the subcategory
+        
+        #get the index of the subcategory i.e the index for the element after the base category
         index = split_link.index(base_category) + 1
+        
         #store the subcategory in a variable
         subcategory = split_link[index]
+
         #if the subcategory is already in the dictionary, append the data to the list
         #if not, create a new key with the subcategory and the value is a list with the data
         if subcategory in subcategoryDict:
@@ -46,5 +51,5 @@ with open("data.csv", "r") as datafile:
         df.to_csv(f"{matching_key}.csv", index=False)
     #if the key is not found, print a message and do not create a file
     else:
-        print(f"Subcategory {matching_key} not found in the file")
+        print(f"Subcategory {subcategory} not found in the file")
         print(f"No file was created")
